3c5fd1a259df1478c9d4ff175092dfc07af137a7
Pedro <pedro.ruizcampillo@icloud.com>
2024-01-06 11:06:06 +0100

    switched to poetry, added status endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM python:3.10
+FROM python:3.12
 
 WORKDIR /app
 
-RUN pip install pipenv
+RUN pip install poetry
 
-COPY server.py utils.py router.py ./Pipfile* /app/
+COPY server.py utils.py router.py poetry.lock pyproject.toml /app/
 
-RUN pipenv install --system --deploy --ignore-pipfile
+RUN poetry config virtualenvs.create false && poetry install --no-dev
 
-CMD ["python", "server.py"]
+CMD ["poetry", "run", "python", "server.py"]
--- server.py
+++ server.py
@@ -1,14 +1,9 @@
-import os
-
 import uvicorn
 from fastapi import Depends, FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 
 from router import router
 from utils import validate_request
-
-X_SECRET = os.getenv("X-SECRET")
-
 
 app = FastAPI()
 
@@ -29,6 +24,11 @@ async def catch_exceptions(request: Request, call_next):
         return await call_next(request)
     except Exception as e:
         return Response(f"error {e}", status_code=500)
+
+
+@app.get("/status")
+def status():
+    return {"status": "ok"}
 
 
 def main():
